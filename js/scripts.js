de5b163ccee0893a700246db762a5733c9123be0
praxyfarhana <praxyfarhana@gmail.com>
2018-07-20 16:15:23 +0300

    created variables

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ function Game (player1,player2) {
 //front-end
 $(document).ready(function() {
   $("form#game").submit(function(event) {
+    var InputtedPlayer1 = $("input#pla1").val();
+    var InputtedPlayer2 = $("inpit#pla2").val();
+    var newGame = new Game (InputtedPlayer1,InputtedPlayer2)
 
-  })
-})
+  });
+});
